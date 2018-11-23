56630747b828b658fbaea63b867433de7d0538d3
ogozuacik <ogozuacik@gmail.com>
2018-11-23 03:22:59 +0300

    Delete gaussmvnpdf.m

--- gaussmvnpdf.m
+++
@@ -1,5 +0,0 @@
-%Multivariate Gaussian Probability Density Function
-function gaussPdf = gaussmvnpdf(X, mu, sigma)
-    Xmu = X-mu;
-    gaussPdf = 1/sqrt(det(sigma)*(2*pi)^2) * exp(-0.5*diag(Xmu*inv(sigma)*Xmu'));
-end
